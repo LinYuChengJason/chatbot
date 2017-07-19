58e2d4cf0fd46fe14e18dc4aa9c4565f6d1e406e
LinYuChengJason <a0936658981@gmail.com>
2017-07-19 15:40:19 +0800

    z

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express'); //require為使用那些模組
 var mongodb = require('mongodb'); //使用模組mongodb
+var linebot = require('linebot');
 var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
+
+var bot = linebot({
+	channel : 1511016044,
+	channelSecret : 614a1dc79eaefd4ca0c37263634be761,
+	channelAccessToken : TYdm9aLp06Z+QIsCrCTPGPGrt8XrNx2QpWJFI4z+FbTuhxV2/nucvHZo7+kkdPlY1EowYjAd1CSDu8sqRL3G0VJl1ks1MRhogtDDITHyz6E4qSL9GMfkyexOCdrZIRLR/gobgmdQEFQvm473Yu0m0QdB04t89/1O/w1cDnyilFU=
+});
+
+const linebotPraser = bot.parser();
+app.get('/line/test',linebotParser);
 
 var mongodbURL =
 'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; //將MongoDB的位置在Server程式碼中以一個變數儲存
