7d4d590bde6020a41dcac05e84ba4606886b9fcb
LinYuChengJason <a0936658981@gmail.com>
2017-08-23 11:42:12 +0800

    Z

--- index.js
+++ index.js
@@ -53,6 +53,43 @@ app.get('/api/test', function(request, response){ //連接到/api/test才會做�
    });
 });
 
+var http = require('http');
+// var apiai = require("../module/apiai");
+var apiai = require("apiai")
+
+var api = apiai("6fde14984d7c4f4d821aa8bf92c3c989");
+
+var server = http.createServer(function(request, response) {
+    if (request.method = 'POST' && request.url == '/upload') {
+        // var outStream = fs.createWriteStream('qwe.wav');
+        var voiceRequest = api.voiceRequest();
+
+        voiceRequest.on('response', function(_response) {
+            response.end(JSON.stringify(_response));
+            // var json = JSON.stringify({'resolvedQuery': _response['result']['resolvedQuery']})
+            // response.end(json);
+        });
+
+        voiceRequest.on('error', function(error) {
+            console.log(error);
+            response.end();
+        });
+
+        request.on('data', function(chunk) {
+            voiceRequest.write(chunk);
+        });
+
+        request.on('end', function() {
+            voiceRequest.end();
+        });
+    } else {
+        response.writeHead(code, {});
+        response.end();
+    }
+
+    console.log(request.headers);
+});
+
 
 app.listen(process.env.PORT || 5000);
 console.log('port ' + (process.env.PORT || 5000)); //啟動伺服器，聆聽port 5000。預設為80port，所以多半被別人佔走。IP:127.0.0.1:5000，domain:http://localhost:5000
