2d03611af8cc6976e7fc1660bb5420a3e5d7ea47
LinYuChengJason <a0936658981@gmail.com>
2017-08-29 16:32:31 +0800

    Z

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ request.on('error', function(error) {
  
 request.end();
 
-/*var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
+var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
 
-var bot = linebot({
+/*var bot = linebot({
   "channelId": "1531669581",
   "channelSecret": "a990b2c5396e8e5c207db5e034d74711",
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
