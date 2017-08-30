8ff2db145236941ee015ef7578c89f593ea33d5a
LinYuChengJason <a0936658981@gmail.com>
2017-08-30 17:32:05 +0800

    Z

--- index.js
+++ index.js
@@ -1,60 +1,84 @@
 var express = require('express'); //require為使用那些模組
-var bodyParser = require('body-parser');
+var mongodb = require('mongodb'); //使用模組mongodb
+var linebot = require('linebot'); //使用模組linebot
+var apiai = require('apiai');
 var request = require('request');
 
 var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(process.env.PORT || 5000);
-console.log('port ' + (process.env.PORT || 5000)); //啟動伺服器，聆聽port 5000。預設為80port，所以多半被別人佔走。IP:127.0.0.1:5000，domain:http://localhost:5000
-
-/* For Facebook Validation */
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'Jason') {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    res.status(403).end();
-  }
-});
-
-/* Handling all messenges */
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
-  }
-});
+var apiaiAPP = apiai('96499911855b40b29cc7908eca2ed768');
 
 function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: EAAEMiM9fx78BAOS3qgJd4Xvlcj9ijZAAefTWUrwHYE59xvH4UNpnBZBhZCJZAUfHSIM8skn6FkHJa5b2EyjJmvY22nAT6XTYf464mPl9L5CUAwMuP46rbCyykxzZBxN9ZCfOHkkoKBsoGVNzrLh8EoKhfJWZAxZCLPrkXz9WSF5dgyjfGiOvmb9P},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'Jason' // use any arbitrary id
   });
+
+  apiai.on('response', (response) => {
+    // Got a response from api.ai. Let's POST to Facebook Messenger
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
 }
 
+
+var bot = linebot({
+  "channelId": "1531669581",
+  "channelSecret": "a990b2c5396e8e5c207db5e034d74711",
+  "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
+}); // 連接line
+
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});  //使用者打甚麼，LINE回什麼
+
+const linebotParser = bot.parser();
+
+app.post('/', linebotParser);  //路徑
+
+var mongodbURL =
+'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; //將MongoDB的位置在Server程式碼中以一個變數儲存
+
+var myDB; //建立一個全域變數myDB
+mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
+	if(err){                                               //事件監聽器用在非同步程式碼，不確定何時會用到
+		console.log(err);                                  //若回傳的參數有error，用console.log()印出錯誤內容
+	} else{
+		myDB = db;                                         //在mongoDB成功連線後，留住db物件
+		console.log('connection success');                 //若沒有錯誤表示連線成功，印出connection success
+	}
+});
+
+app.get('/api/test', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
+	var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
+	collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
+		if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
+			response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
+		} else{                                      //.end()為將資料回傳給使用者
+			response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
+			response.status(200).send(docs);
+			response.end();
+		}
+   });
+});
+
+app.listen(process.env.PORT || 5000);
+console.log('port ' + (process.env.PORT || 5000)); //啟動伺服器，聆聽port 5000。預設為80port，所以多半被別人佔走。IP:127.0.0.1:5000，domain:http://localhost:5000
 
 
 
