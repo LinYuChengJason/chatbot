623b1a7fe90fd9a61a4bff1671dbf53e7ad4dc60
LinYuChengJason <a0936658981@gmail.com>
2017-08-30 14:20:06 +0800

    Z

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var request = require('request');
 
 var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
 
-
-app.get('/api/test', function(request, response){
 var apiaiAPP = apiai('96499911855b40b29cc7908eca2ed768');
 
 function sendMessage(event) {
@@ -32,7 +30,7 @@ function sendMessage(event) {
 )
 
 
-/*var bot = linebot({
+var bot = linebot({
   "channelId": "1531669581",
   "channelSecret": "a990b2c5396e8e5c207db5e034d74711",
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
@@ -53,7 +51,7 @@ bot.on('message', function(event) {
 
 const linebotParser = bot.parser();
 
-app.post('/', linebotParser);  //路徑*/
+app.post('/', linebotParser);  //路徑
 
 var mongodbURL =
 'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; //將MongoDB的位置在Server程式碼中以一個變數儲存
