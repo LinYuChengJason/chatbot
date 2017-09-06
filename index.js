cecb35d305bb03c91a8d9331a37160597e71344e
LinYuChengJason <a0936658981@gmail.com>
2017-09-06 17:18:09 +0800

    Z

--- index.js
+++ index.js
@@ -40,47 +40,15 @@ app.post('/', linebotParser);
 app.use(bodyParser.json());
 
 app.post('/webhook', function (req, res) {
-
-    console.log('webhook request');
-
-    try {
-        var speech = 'empty speech';
-
-        if (req.body) {
-            var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }
-});
+	console.log(req.body);
+	var result = {
+	"speech": "",
+    "source": "",
+    "displayText": "晴天 action: apiai-webhook-sample"
+	}
+	res.send(result);
+}
+)
 
 /*bot.on('message', function(event) {
   if (event.message.type = 'text') {
