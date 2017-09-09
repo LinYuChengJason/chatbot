de803b1e5eb20583450e4c3fca61fc80c042dce0
LinYuChengJason <a0936658981@gmail.com>
2017-09-09 16:28:35 +0800

    Z

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', function(event) {
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
+      console.log(response);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
