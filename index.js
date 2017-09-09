06597441769095a2eb04b18cb17a0809f08f672f
LinYuChengJason <a0936658981@gmail.com>
2017-09-09 10:07:33 +0800

    Z

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', function (event) {
 });
 
 var linebotParser = bot.parser();
-app.post('/linewebhook', linebotParser);
+app.post('/', linebotParser);
 
 app.use(bodyParser.json());
 
