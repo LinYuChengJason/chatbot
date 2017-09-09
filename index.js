5f6be27f2ab1aba0182acf2e3a08d9585af72c92
LinYuChengJason <a0936658981@gmail.com>
2017-09-09 10:01:49 +0800

    Z

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ var bot = linebot({
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
 }); // 連接line，驗證
 
-bot.on('message', function(event) {
-  console.log(event); //把收到訊息的 event 印出來看看
+bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+        // success 
+    }).catch(function (error) {
+        // error 
+    });
 });
 
 var linebotParser = bot.parser();
