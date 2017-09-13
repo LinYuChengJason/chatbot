12ccfc5bdcf5284165a0ac8424acf65eb4dea6ee
LinYuChengJason <a0936658981@gmail.com>
2017-09-13 12:13:12 +0800

    Z

--- index.js
+++ index.js
@@ -52,9 +52,8 @@ request.on('error', function(error) {
  
 request.end();*/
 
-
 app.post('/webhook', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters
     return res.json({
         speech: speech,
         displayText: speech,
