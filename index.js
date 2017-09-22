b2369c38cd46a60dc8557e81732556421de888a0
LinYuChengJason <a0936658981@gmail.com>
2017-09-22 15:25:53 +0800

    Z

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var bot = linebot({
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
 }); // 連接line，驗證
 
-/*bot.on('message', function(event) {
+bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
@@ -30,7 +30,7 @@ var bot = linebot({
       console.log('錯誤產生，錯誤碼：'+error);
     });
   }
-});*/
+});
 
 /*bot.on('message', function(event) {
   console.log(event); //把收到訊息的 event 印出來看看
