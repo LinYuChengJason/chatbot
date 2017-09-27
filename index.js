d4d380514476c606a0d324c8500529442d7e1455
LinYuChengJason <a0936658981@gmail.com>
2017-09-27 16:43:18 +0800

    Z

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ var bot = linebot({
 
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
-    var msg = response;
+    var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(HELLO);
+      console.log(response);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
