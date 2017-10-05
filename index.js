9aeb58d452b118a17d14e1eadb6d37c804d1ed29
LinYuChengJason <a0936658981@gmail.com>
2017-10-05 18:10:08 +0800

    zz

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
-      // 傳送訊息成功時，可在此寫程式碼 
-	  
-var api = apiai("96499911855b40b29cc7908eca2ed768");
+  
+  var api = apiai("96499911855b40b29cc7908eca2ed768");
  
  var request = api.textRequest( '天氣如何', {
     sessionId: 'Jason'
@@ -40,12 +38,16 @@ request.on('error', function(error) {
 })
  
 request.end();
+
+    // event.reply(msg).then(function(data) {
+       // 傳送訊息成功時，可在此寫程式碼 
+	 
 	  
-     // console.log(msg);
-    }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
-      console.log('錯誤產生，錯誤碼：'+error);
-    });
+      // console.log(msg);
+    // }).catch(function(error) {
+       // 傳送訊息失敗時，可在此寫程式碼 
+      // console.log('錯誤產生，錯誤碼：'+error);
+    // });
   }
 });
 
