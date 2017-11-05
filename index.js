42d25f796d6590ed42729d2cabf6e0be559d6964
LinYuChengJason <a0936658981@gmail.com>
2017-11-05 20:29:54 +0800

    Z

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var express = require('express'); //require為使用模組
+//require為使用模組
+var express = require('express'); 
 var linebot = require('linebot'); 
 var apiai = require('apiai');
 
-var app = express(); //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
+//建立express實體，將express初始化，去NEW一個express，變數app才是重點。
+var app = express(); 
 
 var api = apiai("96499911855b40b29cc7908eca2ed768");
 
+// 連接line，驗證
 var bot = linebot({
   "channelId": "1531669581",
   "channelSecret": "a990b2c5396e8e5c207db5e034d74711",
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
-}); // 連接line，驗證
+}); 
 
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
      msg = event.message.text;
+
+	var request = api.textRequest('text', {
+	    sessionId: 'Jason'
+	});
+	 
+	request.on('response', function(response) {
+	    console.log(response);
+	});
+	 
+	request.on('error', function(error) {
+	    console.log(error);
+	});
+	 
+	request.end();
+
   // 收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
@@ -28,7 +46,8 @@ bot.on('message', function(event) {
 
 var linebotParser = bot.parser();
 
-app.post('/', linebotParser);  //路徑 
+//路徑
+app.post('/', linebotParser);   
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
