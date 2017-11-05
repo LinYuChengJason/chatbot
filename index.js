1bb6ad5bc2415bd3d76ef37c54e58a2d893a2074
LinYuChengJason <a0936658981@gmail.com>
2017-11-05 21:15:15 +0800

    Z

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ bot.on('message', function(event) {
 	request.on('response', function(response) {
   var action = response.result.action;    
   var aiSpeech = response.result.fulfillment.speech;
-  
+
   // 收到文字訊息時，直接把收到的訊息傳回去
     event.reply(aiSpeech).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
+      console.log(aiSpeech);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
@@ -41,10 +41,7 @@ bot.on('message', function(event) {
 	});
 	 
 	request.end();
-
-
-  }
-);
+});
 
 var linebotParser = bot.parser();
 
