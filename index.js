4d9357dda22567e971bf1fba654606d59ca016e1
LinYuChengJason <a0936658981@gmail.com>
2017-11-05 20:50:48 +0800

    z

--- index.js
+++ index.js
@@ -16,14 +16,23 @@ var bot = linebot({
 }); 
 
 bot.on('message', function(event) {
-  if (event.message.type = 'text') {
-     msg = event.message.text;
 
 	var request = api.textRequest('text', {
 	    sessionId: '<Jason>'
 	});
 	 
 	request.on('response', function(response) {
+  var action = response.result.action;    
+  var aiSpeech = response.result.fulfillment.speech;
+  
+  // 收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(aiSpeech).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(msg);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
 	    console.log(response);
 	});
 	 
@@ -33,16 +42,9 @@ bot.on('message', function(event) {
 	 
 	request.end();
 
-  // 收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
-      // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
-    }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
-      console.log('錯誤產生，錯誤碼：'+error);
-    });
+
   }
-});
+);
 
 var linebotParser = bot.parser();
 
