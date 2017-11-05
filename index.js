1907252c5679ee289ee305dccde19f830830af01
LinYuChengJason <a0936658981@gmail.com>
2017-11-05 19:57:51 +0800

    Z

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
      msg = event.message.text;
   // 收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(abc).then(function(data) {
+    event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(abc);
+      console.log('msg');
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
