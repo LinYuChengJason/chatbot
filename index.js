8fae18711d06d499b232878c12bbcfc934b10b70
LinYuChengJason <a0936658981@gmail.com>
2017-11-06 19:49:13 +0800

    Z

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ bot.on('message', function(event) {
 	 
 	request.on('response', function(response) {
 
-	console.log('request success');
   	var action = response.result.action;    
   	var aiSpeech = response.result.fulfillment.speech;
-
+  	if (action == 'weather') {
   // 收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
+    event.reply(aiSpeech).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
-    }).catch(function(error) {
+      console.log(aiSpeech);
+    })
+	}.catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
     });
