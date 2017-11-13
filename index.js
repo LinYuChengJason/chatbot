d0eef3f513c1ceea8cb7934d25b98aca82de956f
LinYuChengJason <a0936658981@gmail.com>
2017-11-13 23:31:08 +0800

    Z

--- index.js
+++ index.js
@@ -15,16 +15,6 @@ var bot = linebot({
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
 }); 
 
-app.get('/test', function(request, response){ 
-		
-		if(err){                                     
-			response.status(406).end();                    
-		} else{                                      			     
-			response.status(200).send('Succeed');
-			response.end();
-		}
-   });
-
 //bot收到訊息後
 bot.on('message', function(event) {
 
