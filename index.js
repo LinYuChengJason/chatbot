14956112f3392dd91dbd5698b64ee1e5be8d115e
LinYuChengJason <a0936658981@gmail.com>
2017-11-15 00:00:16 +0800

    Z

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var bot = linebot({
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
 }); 
 
-// bot.on('message', function(event) {
+bot.on('message', function(event) {
 
 	var text = event.message.text;
 
@@ -44,7 +44,7 @@ var bot = linebot({
 	});
 	 
 	request.end();
-// });
+});
 
 var linebotParser = bot.parser();
 
