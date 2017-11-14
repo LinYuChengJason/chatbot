bffccee2ba16ff869866d200ecceda9aa4da2226
LinYuChengJason <a0936658981@gmail.com>
2017-11-15 03:25:37 +0800

    Z

--- index.js
+++ index.js
@@ -19,64 +19,64 @@ var bot = linebot({
 var mongodbURL =
 'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; //將MongoDB的位置在Server程式碼中以一個變數儲存
 
-var myDB; //建立一個全域變數myDB
-mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
-  if(err){                                               //事件監聽器用在非同步程式碼，不確定何時會用到
-    console.log(err);                                  //若回傳的參數有error，用console.log()印出錯誤內容
-  } else{
-    myDB = db;                                         //在mongoDB成功連線後，留住db物件
-    console.log('connection success');                 //若沒有錯誤表示連線成功，印出connection success
-  }
-});
-
-// bot.on('message', function(event) {
-
-//   var text = event.message.text;
-
-//   var request = api.textRequest(text, {
-//       sessionId: '<Jason>'
-//   });
-   
-//   request.on('response', function(response) {
-
-//     var action = response.result.action;    
-//     var aiSpeech = response.result.fulfillment.speech;
-//     if (action == 'movie') {
-//   // 收到文字訊息時，直接把收到的訊息傳回去
-//     event.reply(aiSpeech).then(function(data) {
-//       // 傳送訊息成功時，可在此寫程式碼 
-//       console.log(aiSpeech);
-//     }).catch(function(error) {
-//       // 傳送訊息失敗時，可在此寫程式碼 
-//       console.log('錯誤產生，錯誤碼：'+error);
-//     });
-//       console.log(response);
-//   }});
-
-//   request.on('error', function(error) {
-//       console.log(error);
-//   });
-   
-//   request.end();
+// var myDB; //建立一個全域變數myDB
+// mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
+//   if(err){                                               //事件監聽器用在非同步程式碼，不確定何時會用到
+//     console.log(err);                                  //若回傳的參數有error，用console.log()印出錯誤內容
+//   } else{
+//     myDB = db;                                         //在mongoDB成功連線後，留住db物件
+//     console.log('connection success');                 //若沒有錯誤表示連線成功，印出connection success
+//   }
 // });
 
-// var linebotParser = bot.parser();
+bot.on('message', function(event) {
 
-// //路徑
-// app.post('/', linebotParser);   
+  var text = event.message.text;
 
-app.post('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
-  var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
-  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
-    if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
-      response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
-    } else{                                      //.end()為將資料回傳給使用者
-      response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
-      response.status(200).send(docs);
-      response.end();
-    }
-   });
+  var request = api.textRequest(text, {
+      sessionId: '<Jason>'
+  });
+   
+  request.on('response', function(response) {
+
+    var action = response.result.action;    
+    var aiSpeech = response.result.fulfillment.speech;
+    if (action == 'movie') {
+  // 收到文字訊息時，直接把收到的訊息傳回去
+    event.reply(aiSpeech).then(function(data) {
+      // 傳送訊息成功時，可在此寫程式碼 
+      console.log(aiSpeech);
+    }).catch(function(error) {
+      // 傳送訊息失敗時，可在此寫程式碼 
+      console.log('錯誤產生，錯誤碼：'+error);
+    });
+      console.log(response);
+  }});
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+   
+  request.end();
 });
+
+var linebotParser = bot.parser();
+
+//路徑
+app.post('/', linebotParser);   
+
+// app.post('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
+//   var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
+//   collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
+//     if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
+//       response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
+//     } else{                                      //.end()為將資料回傳給使用者
+//       response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
+//       response.status(200).send(docs);
+//       response.end();
+//     }
+//    });
+// });
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
