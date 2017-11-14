5bd8b947f28e3f6ca41cbd7b6b8460bf76da90de
LinYuChengJason <a0936658981@gmail.com>
2017-11-15 03:52:07 +0800

    Z

--- index.js
+++ index.js
@@ -31,57 +31,57 @@ mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.Mongo
 });
 var linebotParser = bot.parser();
 
-// app.post('/database', linebotParser); 
+app.post('/database', linebotParser); 
 
-// // app.get('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
-// bot.on('message', function(event) {
-//   var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
-//   collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
-
-//      event.reply(docs).then(function(data) {
-//       // 傳送訊息成功時，可在此寫程式碼 
-//       console.log(docs);
-//     }).catch(function(error) {
-//       // 傳送訊息失敗時，可在此寫程式碼 
-//       console.log('錯誤產生，錯誤碼：'+error);
-//     });
-
-//    });
-// });
-
-//路徑
-app.post('/', linebotParser);  
-
+// app.get('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
 bot.on('message', function(event) {
+  var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
+  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
 
-	var text = event.message.text;
-
-	var request = api.textRequest(text, {
-	    sessionId: '<Jason>'
-	});
-	 
-	request.on('response', function(response) {
-
-  	var action = response.result.action;    
-  	var aiSpeech = response.result.fulfillment.speech;
-  	if (action == 'movie') {
-  // 收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(aiSpeech).then(function(data) {
+     event.reply(docs).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(aiSpeech);
+      console.log(docs);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
     });
-	    console.log(response);
-	}});
 
-	request.on('error', function(error) {
-	    console.log(error);
-	});
-	 
-	request.end();
+   });
 });
+
+// //路徑
+// app.post('/', linebotParser);  
+
+// bot.on('message', function(event) {
+
+// 	var text = event.message.text;
+
+// 	var request = api.textRequest(text, {
+// 	    sessionId: '<Jason>'
+// 	});
+	 
+// 	request.on('response', function(response) {
+
+//   	var action = response.result.action;    
+//   	var aiSpeech = response.result.fulfillment.speech;
+//   	if (action == 'movie') {
+//   // 收到文字訊息時，直接把收到的訊息傳回去
+//     event.reply(aiSpeech).then(function(data) {
+//       // 傳送訊息成功時，可在此寫程式碼 
+//       console.log(aiSpeech);
+//     }).catch(function(error) {
+//       // 傳送訊息失敗時，可在此寫程式碼 
+//       console.log('錯誤產生，錯誤碼：'+error);
+//     });
+// 	    console.log(response);
+// 	}});
+
+// 	request.on('error', function(error) {
+// 	    console.log(error);
+// 	});
+	 
+// 	request.end();
+// });
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
