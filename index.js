d4e1354cb03376f50b8abd41add91eda956ed172
LinYuChengJason <a0936658981@gmail.com>
2017-11-15 13:38:44 +0800

    Z

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var linebot = require('linebot');
 var apiai = require('apiai');
 
 //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
-var app = express();
+var app = express(); 
+
 var api = apiai("96499911855b40b29cc7908eca2ed768");
 
 // 連接line，驗證
@@ -14,39 +15,11 @@ var bot = linebot({
   "channelAccessToken": "OTBP0oDhpEORLXeEi7dgGbROpakoaKRbB4b4p9O2WuXgP/+3KLkohEBC0gE20ayjidJ3Ja4QSmJNwchLiuqsTDnKOMD5CBwKCZ6Bwjbosu5l9kYryfY+5xO1K1chLWdN1LRZRT7By00apZS8mnUZCAdB04t89/1O/w1cDnyilFU="
 }); 
 
-//將MongoDB的位置在Server程式碼中以一個變數儲存
-var mongodbURL =
-'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; 
-
-//建立一個全域變數myDB
-var myDB; 
-
-mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
-  if(err){                                               //事件監聽器用在非同步程式碼，不確定何時會用到
-    console.log(err);                                  //若回傳的參數有error，用console.log()印出錯誤內容
-  } else{
-    myDB = db;                                         //在mongoDB成功連線後，留住db物件
-    console.log('connection success');                 //若沒有錯誤表示連線成功，印出connection success
-  }
-});
-
-app.get('/api/test', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
-  var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
-  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
-    if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
-      response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
-    } else{                                      //.end()為將資料回傳給使用者
-      response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
-      response.status(200).send(docs);
-      response.end();
-    }
-   });
-});
-
 bot.on('message', function(event) {
 
 	var text = event.message.text;
-  var request = api.textRequest(text, {
+
+	var request = api.textRequest(text, {
 	    sessionId: '<Jason>'
 	});
 	 
@@ -74,6 +47,7 @@ bot.on('message', function(event) {
 });
 
 var linebotParser = bot.parser();
+
 //路徑
 app.post('/', linebotParser);   
 
