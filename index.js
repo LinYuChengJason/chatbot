94b326230b10eafdfd315dfa332a8becbd7990bf
LinYuChengJason <a0936658981@gmail.com>
2017-11-18 01:32:33 +0800

    Z

--- index.js
+++ index.js
@@ -2,34 +2,36 @@
 var express = require('express'); 
 var linebot = require('linebot'); 
 var apiai = require('apiai');
-var crawler = require("js-crawler");
-var cheerio = require('cheerio');
-var _ = require('lodash');
-var promise = require('promise');
+var mongodb = require('mongodb'); //使用模組mongodb
 
 //建立express實體，將express初始化，去NEW一個express，變數app才是重點。
 var app = express(); 
 
-// crawler.crawl({
-//   url: `http://www.vscinemas.com.tw/visPrintShowTimes.aspx?cid=${_theaterId}&visLang=2`,
+var mongodbURL =
+'mongodb://LinYuCheng:a0936662285@ds143081.mlab.com:43081/jasondatabase'; //將MongoDB的位置在Server程式碼中以一個變數儲存
 
-//   success: (page) => {
-//     const html = page.content.toString()
-//     const $ = Cheerio.load(html)
-//     let tables = $('.PrintShowTimesFilm').parent().parent().parent().find('table')
-//     let showtimes = []
-//     _.map(tables, (table, idx) => {
-//       let title = $(table).find('.PrintShowTimesFilm').text()
-//         // ...
+var myDB; //建立一個全域變數myDB
+mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.MongoClient的方法connect()進行連線
+  if(err){                                               //事件監聽器用在非同步程式碼，不確定何時會用到
+    console.log(err);                                  //若回傳的參數有error，用console.log()印出錯誤內容
+  } else{
+    myDB = db;                                         //在mongoDB成功連線後，留住db物件
+    console.log('connection success');                 //若沒有錯誤表示連線成功，印出connection success
+  }
+});
 
-//     })
-//     resolve(showtimes)
-//   },
-//   failure: (page) => {
-//     console.log(`Get Showtimes Failed on theater: ${_theaterId}`)
-//     reject([])
-//   }
-// })
+app.get('database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
+  var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
+  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
+    if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
+      response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
+    } else{                                      //.end()為將資料回傳給使用者
+      response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
+      response.status(200).send(docs);
+      response.end();
+    }
+   });
+});
 
 var api = apiai("96499911855b40b29cc7908eca2ed768");
 
