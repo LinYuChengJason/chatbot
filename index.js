ccff3578bf6deba55cd81584f6deef794521d5f6
LinYuChengJason <a0936658981@gmail.com>
2017-12-15 15:57:57 +0800

    Z

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ mongodb.MongoClient.connect(mongodbURL, function(err, db){ //使用mongodb.Mongo
   }
 });
 
-// app.get('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
-//   var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
-//   collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
-//     if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
-//       response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
-//     } else{                                      //.end()為將資料回傳給使用者
-//       response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
-//       response.status(200).send(docs);
-//       console.log('Succeed');
-//       response.end();
-//     }
-//    });
-// });
+app.get('/database', function(request, response){ //連接到/api/test才會做的事情，request帶有連接進來的資訊(參數)，response為回傳的內容。
+  var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
+  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
+    if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
+      response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
+    } else{                                      //.end()為將資料回傳給使用者
+      response.type('application/json');       //沒有錯誤回傳狀態碼200並附帶著資料，因為MongoDB存的資料就是JSON，所以不用特別轉換
+      response.status(200).send(docs);
+      console.log('Succeed');
+      response.end();
+    }
+   });
+});
 
 var api = apiai("96499911855b40b29cc7908eca2ed768");
 
@@ -57,7 +57,7 @@ bot.on('message', function(event) {
   	var aiSpeech = response.result.fulfillment.speech;
   	if (action == '電影時刻表') {
   		var collection = myDB.collection('data'); //使用myDB的方法collection('data')取得data這個collection
-		  collection.find({}, {user : Jason}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
+		  collection.find({}).toArray(function(err, docs){ //使用collection的方法find()取得資料表內的內容，{}表示取得全部內容
 		    if(err){                                     //使用toArray()將資料轉成陣列，function的docs是轉成陣列後的結果
 		      response.status(406).end();              //轉陣列過程若有err，回傳給錯誤碼406，此為Http協定狀態碼      
 		    } else{                                      //.end()為將資料回傳給使用者
